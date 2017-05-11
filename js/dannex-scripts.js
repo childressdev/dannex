fc1fd74b4f2a168b6ee5d2083509ce26457f4d3d
childressdev <jcampbell@childressagency.com>
2017-05-11 15:03:02 -0400

    end of day push
    Header done

--- js/dannex-scripts.js
+++ js/dannex-scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function($){
   $(window).scroll(function(){
-    if($('.hero')[0].getBoundingClientRect().bottom < 0){
+    if($('.hero')[0].getBoundingClientRect().bottom < 50){
       $('nav.navbar').addClass('navbar-mini');
     }
     else{
