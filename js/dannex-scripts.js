9f0901d9737c4a44b6007f44acba4931435b1bb8
childressdev <jcampbell@childressagency.com>
2017-05-12 11:36:12 -0400

    remodel section
    trying to get blocks css right

--- js/dannex-scripts.js
+++ js/dannex-scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function($){
   $(window).scroll(function(){
-    if($('.hero')[0].getBoundingClientRect().bottom < 50){
+    if($('.hero')[0].getBoundingClientRect().bottom < 100){
       $('nav.navbar').addClass('navbar-mini');
     }
     else{
